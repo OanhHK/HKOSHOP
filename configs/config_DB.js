b7b3767f6bd27bc014954439e5e3aa74757ba921
OanhHK <74394901+OanhHK@users.noreply.github.com>
2020-12-22 00:52:06 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://HKOSHOP:hko@atnshop.h3ufw.mongodb.net/atnshop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://HKOSHOP:hko@atnshop.h3ufw.mongodb.net/atnshop?retryWrites=true&w=majority',
+        dbname : "atnshop",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
